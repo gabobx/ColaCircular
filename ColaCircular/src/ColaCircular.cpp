c0bf976f05f4061c1e24728d51b8b224fab9d5a6
Nico-Berto <nico.berto50@gmail.com>
2021-02-08 17:25:39 -0300

    Agregado de hilos y sus funciones

--- ColaCircular/src/ColaCircular.cpp
+++ ColaCircular/src/ColaCircular.cpp
@@ -2,6 +2,23 @@
 #include <cstdio>
 #include <memory>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <linux/rtc.h>
+#include <string.h> /* for strncpy */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#include <netinet/in.h>
+#include <net/if.h>
+
 template <class T>
 class cola_circular {
 public:
@@ -89,15 +106,119 @@ private:
 	size_t final = 0;
 };
 
+void* horaRTC (void* arg){
+	char fecha_hora[256];
+	unsigned long irq_frec;
+	int fd;
+	struct rtc_time tm;
+
+	printf("\nAcceso al dispositivo RTC via ioctl()\n");
+	/*
+	 * Leer fecha y hora del RTC
+	 */
+	if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
+		printf("Error: no se puede abrir /dev/rtc\n");
+		return NULL;
+	} else {
+		if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
+			printf("Error: ioctl sobre /dev/rtc\n");
+			close(fd);
+			return NULL;
+		} else {
+			printf("Hora: %02d:%02d:%02d\n", tm.tm_hour,
+					tm.tm_min, tm.tm_sec);
+		}
+	}
+	close (fd);
+	return NULL;
+}
+
+void* iPv4 (void* arg){
+	int fd;
+	struct ifreq pred;
+	struct sockaddr_in *ipaddr;
+	char address[INET_ADDRSTRLEN];
+
+	strncpy(pred.ifr_name, "wlp2s0", IFNAMSIZ-1);
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		printf("Error al establecer el socket \n");
+		return NULL;
+	}
+
+	printf("\nLos parámetros de la interfaz son:\n\n");
+
+	if (ioctl(fd, SIOCGIFADDR, &pred) == -1) {
+		printf("Error al obtener la IP\n");
+		close(fd);
+		return NULL;
+	}
+
+	ipaddr = (struct sockaddr_in *)&pred.ifr_addr;
+	if (inet_ntop(AF_INET, &ipaddr->sin_addr, address, sizeof(address)) != NULL) {
+		printf("Ip address: %s\n", address);
+	}
+	return NULL;
+}
+
+void* fechaRTC(void* arg){
+	char fecha_hora[256];
+	unsigned long irq_frec;
+	int fd;
+	struct rtc_time tm;
+
+	printf("\nAcceso al dispositivo RTC via ioctl()\n");
+	/*
+	 * Leer fecha y hora del RTC
+	 */
+	if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
+		printf("Error: no se puede abrir /dev/rtc\n");
+		return NULL;
+	} else {
+		if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
+			printf("Error: ioctl sobre /dev/rtc\n");
+			close(fd);
+			return NULL;
+		} else {
+			printf("fecha: %02d/%02d/%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+		}
+	}
+	close (fd);
+	return NULL;
+}
+
 int main(void)
 {
+	int opcion;
+	pthread_t hilo0, hilo1, hilo2;
 	cola_circular<uint32_t> cola(10);		//Creo una cola de tamaño 10
 	printf("Ocupado: %zu, Tamaño total: %zu\n", cola.get_ocupado(), cola.get_tamano());
 
+	printf ("Opciones\n");
+	printf ("1. Hora de Rtc\n");
+	printf ("2. Fecha del sistema\n");
+	printf ("3. Dirección IP de la computadora\n");
+	scanf("%d",&opcion);
+
+	cola.put(opcion);
+	printf("Valor guardado: %d\n", cola.get());
+
+	pthread_create(&hilo0, NULL, iPv4, NULL);
+	pthread_join(hilo0, NULL);
+
+	pthread_create(&hilo1, NULL, fechaRTC, NULL);
+	pthread_join(hilo1, NULL);
+
+	pthread_create(&hilo2, NULL, horaRTC, NULL);
+	pthread_join(hilo2, NULL);
+
+	/*
 	for (int var = 0; var < 10; ++var) {
 		printf("Guardo en cola el valor: %d\n", var);
 		cola.put(var);
 	}
+
 	printf("Cola llena: %d\n", cola.is_llena());
 	int x = cola.get();
 	printf("Leo primer valor guardado: %d\n", x);
@@ -110,6 +231,6 @@ int main(void)
 		printf("Leo siguiente valor guardado: %d\n", x);
 	}
 	printf("Cola vacia: %d\n", cola.is_vacia());
-
+	 */
 	return 0;
 }

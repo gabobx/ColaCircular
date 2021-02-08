b44d80a4dc9775a872725b6bc4868762f9811a2c
gabriel <gabriel@localhost>
2021-02-08 19:57:55 -0300

    Thread para la interfaz con usuario y loop while

--- ColaCircular/src/ColaCircular.cpp
+++ ColaCircular/src/ColaCircular.cpp
@@ -19,6 +19,7 @@
 #include <netinet/in.h>
 #include <net/if.h>
 
+
 template <class T>
 class cola_circular {
 public:
@@ -73,7 +74,7 @@ public:
 		return tamano_max;
 	}
 
-	size_t get_ocupado() const		//Espacio de la cola get_ocupado
+	size_t get_ocupado() const		//Espacio de la cola ocupado
 	{
 		size_t ocupado = tamano_max;
 		if(!completo)
@@ -106,131 +107,155 @@ private:
 	size_t final = 0;
 };
 
+
+cola_circular<uint32_t> cola(3);		//Cola de tamaño 10 global
+
+
 void* horaRTC (void* arg){
 	char fecha_hora[256];
 	unsigned long irq_frec;
-	int fd;
+	int fd,c;
 	struct rtc_time tm;
 
-	printf("\nAcceso al dispositivo RTC via ioctl()\n");
-	/*
-	 * Leer fecha y hora del RTC
-	 */
-	if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
-		printf("Error: no se puede abrir /dev/rtc\n");
-		return NULL;
-	} else {
-		if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
-			printf("Error: ioctl sobre /dev/rtc\n");
-			close(fd);
-			return NULL;
-		} else {
-			printf("Hora: %02d:%02d:%02d\n", tm.tm_hour,
-					tm.tm_min, tm.tm_sec);
+	while(1){
+		if(!cola.is_vacia()){
+			c=cola.get();
+			if (c==1){
+				printf("\nAcceso al dispositivo RTC via ioctl()\n");
+				/*
+				 * Leer fecha y hora del RTC
+				 */
+				if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
+					printf("Error: no se puede abrir /dev/rtc\n");
+					return NULL;
+				} else {
+					if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
+						printf("Error: ioctl sobre /dev/rtc\n");
+						close(fd);
+						return NULL;
+					} else {
+						printf("Hora: %02d:%02d:%02d\n", tm.tm_hour,
+								tm.tm_min, tm.tm_sec);
+					}
+				}
+				close (fd);
+			}else{
+				cola.put(c);	//Si no era para este thread vuelvo a colocarlo en cola
+			}
 		}
-	}
-	close (fd);
-	return NULL;
-}
-
-void* iPv4 (void* arg){
-	int fd;
-	struct ifreq pred;
-	struct sockaddr_in *ipaddr;
-	char address[INET_ADDRSTRLEN];
-
-	strncpy(pred.ifr_name, "wlp2s0", IFNAMSIZ-1);
-
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd < 0) {
-		printf("Error al establecer el socket \n");
-		return NULL;
-	}
-
-	printf("\nLos parámetros de la interfaz son:\n\n");
-
-	if (ioctl(fd, SIOCGIFADDR, &pred) == -1) {
-		printf("Error al obtener la IP\n");
-		close(fd);
-		return NULL;
-	}
-
-	ipaddr = (struct sockaddr_in *)&pred.ifr_addr;
-	if (inet_ntop(AF_INET, &ipaddr->sin_addr, address, sizeof(address)) != NULL) {
-		printf("Ip address: %s\n", address);
+		sleep(1);
 	}
 	return NULL;
 }
+
 
 void* fechaRTC(void* arg){
 	char fecha_hora[256];
 	unsigned long irq_frec;
-	int fd;
+	int fd,c;
 	struct rtc_time tm;
 
-	printf("\nAcceso al dispositivo RTC via ioctl()\n");
-	/*
-	 * Leer fecha y hora del RTC
-	 */
-	if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
-		printf("Error: no se puede abrir /dev/rtc\n");
-		return NULL;
-	} else {
-		if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
-			printf("Error: ioctl sobre /dev/rtc\n");
-			close(fd);
-			return NULL;
-		} else {
-			printf("fecha: %02d/%02d/%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+	while(1){
+		if(!cola.is_vacia()){
+			c=cola.get();
+			if (c==2){
+				printf("\nAcceso al dispositivo RTC via ioctl()\n");
+				/*
+				 * Leer fecha y hora del RTC
+				 */
+				if ((fd = open("/dev/rtc", O_RDONLY, S_IREAD)) < 0){
+					printf("Error: no se puede abrir /dev/rtc\n");
+					return NULL;
+				} else {
+					if ((ioctl(fd, RTC_RD_TIME, &tm)) != 0){
+						printf("Error: ioctl sobre /dev/rtc\n");
+						close(fd);
+						return NULL;
+					} else {
+						printf("fecha: %02d/%02d/%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+					}
+				}
+				close (fd);
+			}else{
+				cola.put(c);	//Si no era para este thread vuelvo a colocarlo en cola
+			}
 		}
+		sleep(1);
 	}
-	close (fd);
+	return NULL;
+}
+
+void* iPv4 (void* arg){
+	int fd,c;
+	struct ifreq pred;
+	struct sockaddr_in *ipaddr;
+	char address[INET_ADDRSTRLEN];
+
+	while(1){
+		if(!cola.is_vacia()){
+			c=cola.get();
+			if (c==3){
+				strncpy(pred.ifr_name, "wlp2s0", IFNAMSIZ-1);
+
+				fd = socket(AF_INET, SOCK_DGRAM, 0);
+				if (fd < 0) {
+					printf("Error al establecer el socket \n");
+					return NULL;
+				}
+
+				printf("\nLos parámetros de la interfaz son:\n\n");
+
+				if (ioctl(fd, SIOCGIFADDR, &pred) == -1) {
+					printf("Error al obtener la IP\n");
+					close(fd);
+					return NULL;
+				}
+
+				ipaddr = (struct sockaddr_in *)&pred.ifr_addr;
+				if (inet_ntop(AF_INET, &ipaddr->sin_addr, address, sizeof(address)) != NULL) {
+					printf("Ip address: %s\n", address);
+				}
+			}else{
+				cola.put(c);	//Si no era para este thread vuelvo a colocarlo en cola
+			}
+		}
+		sleep(1);
+	}
+	return NULL;
+}
+
+void* interfaz(void* arg){
+	int opcion;
+	while(1){
+
+		printf ("Opciones\n");
+		printf ("1. Hora de Rtc\n");
+		printf ("2. Fecha del sistema\n");
+		printf ("3. Dirección IP de la computadora\n");
+		scanf("%d",&opcion);
+
+		cola.put(opcion);
+		printf("Opción ingresada: %d\n", opcion);
+
+		sleep(1);
+	}
+
 	return NULL;
 }
 
 int main(void)
 {
-	int opcion;
-	pthread_t hilo0, hilo1, hilo2;
-	cola_circular<uint32_t> cola(10);		//Creo una cola de tamaño 10
+	pthread_t hilo0, hilo1, hilo2,hilo3;
 	printf("Ocupado: %zu, Tamaño total: %zu\n", cola.get_ocupado(), cola.get_tamano());
 
-	printf ("Opciones\n");
-	printf ("1. Hora de Rtc\n");
-	printf ("2. Fecha del sistema\n");
-	printf ("3. Dirección IP de la computadora\n");
-	scanf("%d",&opcion);
+	pthread_create(&hilo0, NULL, interfaz, NULL);
+	pthread_create(&hilo1, NULL, horaRTC, NULL);
+	pthread_create(&hilo2, NULL, fechaRTC, NULL);
+	pthread_create(&hilo3, NULL, iPv4, NULL);
 
-	cola.put(opcion);
-	printf("Valor guardado: %d\n", cola.get());
-
-	pthread_create(&hilo0, NULL, iPv4, NULL);
-	pthread_join(hilo0, NULL);
-
-	pthread_create(&hilo1, NULL, fechaRTC, NULL);
-	pthread_join(hilo1, NULL);
-
-	pthread_create(&hilo2, NULL, horaRTC, NULL);
-	pthread_join(hilo2, NULL);
-
-	/*
-	for (int var = 0; var < 10; ++var) {
-		printf("Guardo en cola el valor: %d\n", var);
-		cola.put(var);
+	while(1){
+		sleep(2);
 	}
 
-	printf("Cola llena: %d\n", cola.is_llena());
-	int x = cola.get();
-	printf("Leo primer valor guardado: %d\n", x);
-	printf("Guardado en cola el valor 18 \n");
-	cola.put(18);
-	x = cola.get();
-	printf("Leo segundo valor guardado: %d\n", x);
-	while(!cola.is_vacia()){
-		x = cola.get();
-		printf("Leo siguiente valor guardado: %d\n", x);
-	}
-	printf("Cola vacia: %d\n", cola.is_vacia());
-	 */
 	return 0;
 }
